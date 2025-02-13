35fda0a77ac707c08bc96e3424d643d4a655fd2a
hangyin2020 <hangyin2010@gmail.com>
2025-02-13 14:03:01 -0800

    refactor code and fix tests

--- gilded_rose.py
+++ gilded_rose.py
@@ -20,30 +20,60 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue  # Legendary item does not change
+            
+            if item.name == "Aged Brie":
+                self._update_aged_brie(item)
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                self._update_backstage_passes(item)
+            elif item.name.startswith("Conjured"):
+                self._update_conjured_item(item)
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+                self._update_regular_item(item)
+
+            item.sell_in -= 1
+
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                self._handle_expired_item(item)
+
+    def _update_aged_brie(self, item):
+        self._increase_quality(item)
+    
+    def _update_backstage_passes(self, item):
+        self._increase_quality(item)
+        if item.sell_in < 11:
+            self._increase_quality(item)
+        if item.sell_in < 6:
+            self._increase_quality(item)
+    
+    def _update_conjured_item(self, item):
+        self._decrease_quality(item, amount=2)
+    
+    def _update_regular_item(self, item):
+        self._decrease_quality(item)
+    
+    def _handle_expired_item(self, item):
+        if item.name == "Aged Brie":
+            self._increase_quality(item)
+        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            item.quality = 0  # Expired backstage pass drops to 0
+        elif item.name.startswith("Conjured"):
+            self._decrease_quality(item, amount=2)
+        else:
+            self._decrease_quality(item)
+    
+    def _increase_quality(self, item):
+        if item.quality < 50:
+            item.quality += 1
+    
+    def _decrease_quality(self, item, amount=1):
+        if item.quality > 0:
+            item.quality -= amount
+            if item.quality < 0:
+                item.quality = 0
+
+    def get_item(self):
+        """Returns a list of item names"""
+        return [item.name for item in self.items]
+    
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -22,44 +22,56 @@ class GildedRoseTest(unittest.TestCase):
         all_items = gilded_rose.get_item()
         self.assertEqual(["Sulfuras"], all_items)
 
-    # Logical error test: Aged Brie increases in quality as it gets older
+    # Test: "Sulfuras" should never decrease in quality or sell_in
+    def test_sulfuras_should_not_decrease_quality(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 5, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        sulfuras_item = items[0]
+        self.assertEqual(80, sulfuras_item.quality)  # Quality remains 80
+        self.assertEqual(5, sulfuras_item.sell_in)  # Sell_in remains unchanged
+
+    # Removed non-existent get_item() method test
+
+    # Test: "Aged Brie" increases in quality as it gets older
     def test_aged_brie_increases_in_quality(self):
         items = [Item("Aged Brie", 2, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual(1, items[0].quality)
-        self.assertEqual(1, items[0].sell_in)
+        self.assertEqual(1, items[0].quality)  # Increases by 1
+        self.assertEqual(1, items[0].sell_in)  # Decreases by 1
 
-    # Logical error test: Backstage passes increase in quality as sell_in approaches
+    # Test: "Backstage passes" increase in quality correctly
     def test_backstage_passes_increase_in_quality(self):
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual(22, items[0].quality)
-        self.assertEqual(15, items[0].sell_in)
+        self.assertEqual(21, items[0].quality)  # Increases by 1
+        self.assertEqual(14, items[0].sell_in)  # Decreases by 1
 
-    # Logical error test: Quality of an item is never more than 50
+    # Test: Quality of an item is never more than 50
     def test_quality_never_more_than_50(self):
         items = [Item("Aged Brie", 2, 50)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual(51, items[0].quality)
-        self.assertEqual(1, items[0].sell_in)
+        self.assertEqual(50, items[0].quality)  # Should remain 50
+        self.assertEqual(1, items[0].sell_in)  # Decreases by 1
 
-    # Syntax error test: Calling a non-existent method
+    # Test: Calling a non-existent method raises an AttributeError
     def test_non_existent_method(self):
         items = [Item("Sulfuras, Hand of Ragnaros", 0, 80)]
         gilded_rose = GildedRose(items)
         with self.assertRaises(AttributeError):
             gilded_rose.non_existent_method()
 
-    # Logical error test: This test should fail
+    # Test: Correct expectation for Aged Brie increasing in quality
     def test_fail_this_test(self):
         items = [Item("Aged Brie", 2, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual(2, items[0].quality)  # This assertion is incorrect on purpose
-        self.assertEqual(1, items[0].sell_in)
+        self.assertEqual(1, items[0].quality)  # Fixed incorrect expectation
+        self.assertEqual(1, items[0].sell_in)  # Sell_in decreases by 1
+
 
 
 if __name__ == '__main__':
